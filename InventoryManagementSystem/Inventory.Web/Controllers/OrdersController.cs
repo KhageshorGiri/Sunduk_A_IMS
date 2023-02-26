765f0fd1b5408a710aa9cf0596269d020e8c1534
KhageshorGiri <girikhageshor2432@gmail.com>
2023-02-26 18:52:58 +0545

    templiting dashbord

---
+++ InventoryManagementSystem/Inventory.Web/Controllers/OrdersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Web.Controllers
+{
+    public class OrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
